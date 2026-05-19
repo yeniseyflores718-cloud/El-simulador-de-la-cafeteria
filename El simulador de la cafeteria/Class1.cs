e36a77ccd1ef5429966760a75fdda8f504ffbaaa
Ariadna Yenisey <yeniseyflores718@gamil.com>
2026-05-19 15:58:37 -0600

    Agregar archivos de proyecto.

---
+++ El simulador de la cafeteria/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace El_simulador_de_la_cafeteria
+{
+    internal class bebida
+    {
+        private string nombre;
+        private string tamano;
+        private float precio;
+        private float descuento;
+        public float Descuento
+        {
+            get { return descuento; }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    descuento = value;
+                }
+                else
+                {
+                    descuento = -1;
+                }
+            }
+        }
+        //constructor
+        public bebida(string nombre, string tamano, float precio)
+        {
+            this.nombre = nombre;
+            this.tamano = tamano;
+            this.precio = precio;
+        }
+        public string preparar()
+        {
+            return "preparando un: "+ nombre+ " tamaño: "+ tamano;
+        }
+        // Método para aplicar descuento
+        public void AplicarDescuento(float porcentaje)
+        {
+            // Usar la propiedad para validar
+            Descuento = porcentaje;
+
+            if (Descuento != -1)
+            {
+                float descuentoAplicado = precio * (Descuento / 100);
+                precio = precio - descuentoAplicado;
+
+                Console.WriteLine("Descuento aplicado correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("Error: el descuento debe estar entre 0 y 100.");
+            }
+        }
+        // Método para mostrar descripción
+        public string MostrarDescripcion()
+        {
+            return "Esto es un " + nombre +
+                              " de tamaño " + tamano +
+                              " con un costo de $" + precio;
+        }
+    }
+}
---
+++ El simulador de la cafeteria/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace El_simulador_de_la_cafeteria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string nombre, tamaño;
+            float precio;
+            // Primera bebida
+            
+            Console.WriteLine("Ingrese el nombre de la bebida 1:");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el tamaño:");
+            tamaño = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el precio:");
+            precio = float.Parse(Console.ReadLine());
+
+            bebida bebida1 = new bebida(nombre, tamaño, precio);
+            // Segunda bebida
+            Console.WriteLine("\nIngrese el nombre de la bebida 2:");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el tamaño:");
+            tamaño = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el precio:");
+            precio = float.Parse(Console.ReadLine());
+
+            bebida bebida2 = new bebida(nombre, tamaño, precio);
+            // Tercera bebida
+            Console.WriteLine("\nIngrese el nombre de la bebida 3:");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el tamaño:");
+            tamaño = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el precio:");
+            precio = float.Parse(Console.ReadLine());
+
+            bebida bebida3 = new bebida(nombre, tamaño, precio);
+            bebida1.AplicarDescuento(20);
+            bebida2.AplicarDescuento(10);
+            bebida3.AplicarDescuento(0);
+
+            Console.WriteLine("Preparando....");
+            Console.WriteLine("\n" + bebida1.preparar());
+            Console.WriteLine("\n" + bebida2.preparar());
+            Console.WriteLine("\n" + bebida3.preparar());
+
+
+            Console.WriteLine("\n Descripcion del pedido....");
+            Console.WriteLine("\n" + bebida1.MostrarDescripcion ());
+            Console.WriteLine("\n" + bebida2.MostrarDescripcion());
+            Console.WriteLine("\n" + bebida3.MostrarDescripcion());
+
+            Console.ReadKey();
+        }
+    }
+        
+    }
+
